45a7e02bffe6096cc99457da2abda5e691277838
marisolfrias93-debug <marisol.frias93@icloud.com>
2026-04-29 17:27:58 -0400

    Delete New project 3 directory

--- New project 3/supabase-config.js
+++
@@ -1,4 +0,0 @@
-window.STEADY_STEPS_SUPABASE = {
-  url: "https://xkstbxdcrzqnwixktajs.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhrc3RieGRjcnpxbndpeGt0YWpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0ODUxMDcsImV4cCI6MjA5MzA2MTEwN30.wIDPtE2zyYtrbKgV-W_L-3sjm5Fvy7_ZGZuRdFQFp1k"
-};
